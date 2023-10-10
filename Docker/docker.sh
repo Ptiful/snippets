91f3c1269bea9694f06a6b73fa35069ccf7f333f
Ptiful <strazzu@gmail.com>
2023-10-10 11:46:01 +0200

    renaming files and arranging folders

---
+++ Docker/docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+------------------------------Docker------------------------------
+docker login
+docker build -t app .
+docker run app
+docker image ls
+docker container ls (docker ps)
+docker image rm
+docker stop img/container
+docker system df
+docker container/image prune --force
+docker tag getting-started YOUR-USER-NAME/getting-started
+docker push YOUR-USER-NAME/getting-started
